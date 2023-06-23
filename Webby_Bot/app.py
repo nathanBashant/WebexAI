7d2862ae696e8373a63a3b4ca96016f2e650033f
nathanBashant <97708679+nathanBashant@users.noreply.github.com>
2023-06-23 10:35:00 -0400

    Update app.py

--- Webby_Bot/app.py
+++ Webby_Bot/app.py
@@ -1,7 +1,7 @@
 from webex_bot.webex_bot import WebexBot
 from gpt import gpt
 
-bot = WebexBot("MWFlOTA1NWMtZWZiMS00Y2E4LWE3NjMtMmM5NTZjNjE1MTE5M2IzMzgzMzAtYTRl_PF84_1eb65fdf-9643-417f-9974-ad72cae0e10f")
+bot = WebexBot("API KEY")
 
 bot.commands.clear()
 
